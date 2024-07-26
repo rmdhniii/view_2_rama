47bcebca6f3185d453c71f43cb5d37d199e87654
kelpinds <kelpindwisaputra595@gmail.com>
2024-07-26 15:08:11 -0800

    new

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('evaluasi/jawab/empat/{jurusan}', function ($jurusan) {
+    $profile = [
+        'tkro' => [
+            'nama' => 'Teknik Kendaraan Ringan Otomotif',
+            'kajur' => 'Rivan Indrayan, S.T',
+            'kelas' => 6
+        ],
+        'tp' => [
+            'nama' => 'Teknik Permesinan',
+            'kajur' => 'Siti Hamidah, A.Md',
+            'kelas' => 5
+        ],
+        'rpl' => [
+            'nama' => 'Rekayasa Perangkat Lunak',
+            'kajur' => 'Joko Kristianto, S.T',
+            'kelas' => '3'
+        ],
+        'ak' => [
+            'nama' => 'Akuntansi',
+            'kajur' => 'Yuliati, S.E, M.M',
+            'kelas' => 1
+        ],
+    ];
+    return view('evaluasi.profile', $profile);
+});
+
